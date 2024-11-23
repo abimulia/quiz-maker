b5f5e033c8f42ec81f6912a04c9e3b9f738dd633
Abi Mulia <abimulia@outlook.com>
2024-11-24 01:50:08 +0700

    store highschore to local storage

--- src/app/end/end.component.ts
+++ src/app/end/end.component.ts
@@ -1,13 +1,15 @@
 import { Component, inject, OnInit } from '@angular/core';
-import { FormControl } from '@angular/forms';
+import { FormControl, ReactiveFormsModule } from '@angular/forms';
 import { ActivatedRoute, Router } from '@angular/router';
 import { ButtonModule } from 'primeng/button';
 import { CardModule } from 'primeng/card';
+import { LocalStorageService } from '../shared/local-storage.service';
+import { InputTextModule } from 'primeng/inputtext';
 
 @Component({
   selector: 'app-end',
   standalone: true,
-  imports: [CardModule,ButtonModule],
+  imports: [CardModule,ButtonModule,ReactiveFormsModule,InputTextModule ],
   templateUrl: './end.component.html',
   styleUrl: './end.component.css'
 })
@@ -17,18 +19,63 @@ export class EndComponent implements OnInit{
   router = inject(Router);
   activatedRoute = inject(ActivatedRoute);
   name = new FormControl();
+  highScores: { score: number; name: string }[] = [];
+  private storageKey = 'highScores';
   userRank = 1;
 
+  constructor(private localStorageService: LocalStorageService) {}
+
   ngOnInit(): void {
+    const highScoresData = this.localStorageService.get(this.storageKey);
+    this.highScores = highScoresData ? JSON.parse(highScoresData) : [];
     this.activatedRoute.queryParams.subscribe((params) => {
       const scoreParam = params['score'];
       if (scoreParam) {
         this.score = Number(scoreParam);
       }
     });
-    this.userRank = 1;
-    this.congrats = 'Wow, you really learn a lot';
+    const highscore = {
+      score: this.score,
+      name: "checkRankingScore",
+    };
+    const checkRankArrays = [...this.highScores];
+    checkRankArrays.push(highscore);
+    checkRankArrays.sort((a: any, b: any) => b.score - a.score);
+    const userRanking = checkRankArrays.indexOf(highscore);
+    this.userRank = userRanking + 1;
+    switch (true) {
+      case this.userRank > 10:
+        this.congrats = 'No worries, just add more reading :-)';
+        break;
+      case this.userRank <= 10 && this.userRank > 7:
+        this.congrats = 'Good, great progress';
+        break;
+      case this.userRank <= 7 && this.userRank > 4:
+        this.congrats = 'Wow, you really learn a lot';
+        break;
+      case this.userRank <= 4 && this.userRank > 1:
+        this.congrats = 'Excelent, almost nailed';
+        break;
+        break;
+      case this.userRank == 1:
+        this.congrats = 'Amazing, you are number one!';
+        break;
+      default:
+        break;
+    }
       
+  }
+
+  submitScore() {
+    const highscore = {
+      score: this.score,
+      name: this.name.value,
+    };
+    this.highScores.push(highscore);
+    this.highScores.sort((a: any, b: any) => b.score - a.score);
+    this.highScores.splice(10);
+    this.localStorageService.set(this.storageKey, JSON.stringify(this.highScores));
+    this.router.navigate(['highscores']);
   }
 
   playGame() {
