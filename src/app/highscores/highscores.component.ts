d51332716dc12cf6a3a08ceea85ea606c91c7341
Abi Mulia <abimulia@outlook.com>
2024-11-24 00:25:58 +0700

    add initial highscores page

---
+++ src/app/highscores/highscores.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-highscores',
+  standalone: true,
+  imports: [],
+  templateUrl: './highscores.component.html',
+  styleUrl: './highscores.component.css'
+})
+export class HighscoresComponent {
+
+}
