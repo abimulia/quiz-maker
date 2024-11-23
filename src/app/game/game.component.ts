be7af8b362d2092929ad2d2aa439d7df551b1999
Abi Mulia <abimulia@outlook.com>
2024-11-24 00:24:47 +0700

    add fetching and diplay quiz question

--- src/app/game/game.component.ts
+++ src/app/game/game.component.ts
@@ -1,6 +1,9 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { CardModule } from 'primeng/card';
 import { ProgressBarModule } from 'primeng/progressbar';
+import { FormattedQuestion, Question } from './model/quiz';
+import { GameService } from './service/game.service';
+import { decode } from 'html-entities';
 
 
 @Component({
@@ -10,6 +13,100 @@ import { ProgressBarModule } from 'primeng/progressbar';
   templateUrl: './game.component.html',
   styleUrl: './game.component.css'
 })
-export class GameComponent {
+export class GameComponent implements OnInit{
+  questions: Question[] = [];
+  formattedQuestions: FormattedQuestion[] = [];
+  currentQuestion: FormattedQuestion | undefined;
+  loadingQuestion = false;
+  CORRECT_BONUS = 10;
+  MAX_QUESTIONS = 5;
+  score = 0;
+  questionCounter = 0;
+  progressText = "";
+  progressBar = 0;
+  classToApply = "";
+
+  constructor(private gameService: GameService){}
+
+  
+  ngOnInit(): void {
+      this.loadingQuestion = true;
+      this.getQuestions();
+      setTimeout(() => {
+        this.loadingQuestion = false;
+        this.getNewQuestion();
+      }, 500);
+  }
+
+  startGame(){
+    this.questionCounter = 0;
+    this.score = 0;
+
+  }
+
+  getQuestions(){
+    this.gameService.getQuestions().subscribe((data) => {
+      this.questions = data;
+      this.questions.map((question)=>{
+        const formattedQuestion = this.convertQuestionToFormattedQuestion(question);
+        this.formattedQuestions.push(formattedQuestion);
+      })
+    })
+  }
+
+  getNewQuestion(){
+    if(this.formattedQuestions.length === 0 || this.questionCounter >= this.MAX_QUESTIONS){
+      console.log("End Game");
+    }
+    this.questionCounter++;
+    this.progressText = "Question "+ this.questionCounter + " / " + this.MAX_QUESTIONS;
+    this.progressBar = (this.questionCounter/this.MAX_QUESTIONS)*100;
+    const questionIndex = Math.floor(Math.random() * this.formattedQuestions.length);
+    this.currentQuestion = this.formattedQuestions[questionIndex];
+    this.formattedQuestions.splice(questionIndex,1);
+  }
+
+  submitAnswer(selectedChoice: number){
+    console.log("selectedChoice: " + selectedChoice);
+    if(selectedChoice == this.currentQuestion?.answer){
+      this.score += this.CORRECT_BONUS;
+      this.classToApply = "correct";
+    } else {
+      this.classToApply = "incorrect";
+    }
+    setTimeout(() => {
+      this.classToApply = "";
+      this.getNewQuestion();
+    }, 500);
+
+  }
+
+  private convertQuestionToFormattedQuestion(
+    questionObj: Question
+  ): FormattedQuestion {
+    const choices = [
+      questionObj.correct_answer,
+      ...questionObj.incorrect_answers,
+    ];
+
+
+    const shuffledChoices = this.shuffleArray(choices);
+    return {
+      question: decode(questionObj.question),
+      choice2: decode(shuffledChoices[1]),
+      choice3: decode(shuffledChoices[2]),
+      choice4: decode(shuffledChoices[3]),
+      choice1: decode(shuffledChoices[0]),
+      answer: shuffledChoices.indexOf(questionObj.correct_answer) + 1,
+    };
+
+    
+  }
+
+  private shuffleArray(array: string[]): string[] {
+    return array.sort(() => Math.random() - 0.5);
+  }
+
+  
 
 }
--- src/app/game/service/game.service.ts
+++ src/app/game/service/game.service.ts
@@ -1,9 +1,21 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { map, Observable } from 'rxjs';
+import { Question } from '../model/quiz';
 
 @Injectable({
   providedIn: 'root'
 })
 export class GameService {
+  // private apiUrl = "https://opentdb.com/api.php?amount=10&category=9&difficulty=easy&type=multiple";
+  private apiUrl = "./assets/questions.json";
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  getQuestions(): Observable<Question[]> {
+    return this.http.get<any>(this.apiUrl).pipe(
+      map((response) => 
+        response.results as Question[]) // Cast results to Question[]
+    );
+  }
 }
